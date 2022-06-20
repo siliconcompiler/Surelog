3f9835eb5b118237bbabe26fbddca251fee46767
Noah Moroze <noah@zeroasic.com>
2022-06-20 17:22:56 -0400

    Clean up setup.py metadata

--- setup.py
+++ setup.py
@@ -1,12 +1,22 @@
+import re
+
 from skbuild import setup
+
+def parse_version():
+    REGEX = r'kVersionNumber\("([0-9]+\.[0-9]+)"\)'
+    with open('src/CommandLine/CommandLineParser.cpp', 'r') as f:
+        match = re.search(REGEX, f.read())
+        if match:
+            return match.group(1)
+
+    raise ValueError('Could not find version')
 
 with open("README.md", "r", encoding="utf-8") as readme:
   long_desc = readme.read()
 
 setup(
     name='Surelog',
-
-    version='0.0',
+    version='0.' + parse_version(),
 
     author='Alain Dargelas',
     author_email='',
@@ -24,13 +34,9 @@ setup(
     },
 
     url='https://github.com/chipsalliance/Surelog',
-    # download_url='https://cmake.org/download',
-    # project_urls={
-    #     "Documentation": "https://cmake-python-distributions.readthedocs.io/",
-    #     "Source Code": "https://github.com/scikit-build/cmake-python-distributions",
-    #     "Mailing list": "https://groups.google.com/forum/#!forum/scikit-build",
-    #     "Bug Tracker": "https://github.com/scikit-build/cmake-python-distributions/issues",
-    # },
+    project_urls={
+        "Bug Tracker": "https://github.com/chipsalliance/Surelog/issues",
+    },
 
     description='SystemVerilog 2017 Pre-processor, Parser, Elaborator, UHDM Compiler. ' \
                 'Provides IEEE Design/TB C/C++ VPI and Python AST API.',
@@ -38,22 +44,5 @@ setup(
     long_description=long_desc,
     long_description_content_type="text/markdown",
 
-    classifiers=[
-        'License :: OSI Approved :: Apache Software License',
-        'License :: OSI Approved :: BSD License',
-        'Programming Language :: C',
-        'Programming Language :: C++',
-        'Programming Language :: Fortran',
-        'Programming Language :: Python',
-        'Operating System :: OS Independent',
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools'
-        ],
-
     license='Apache 2.0',
-
-    keywords='CMake build c++ fortran cross-platform cross-compilation',
-
-    # extras_require={"test": test_requirements},
 )
